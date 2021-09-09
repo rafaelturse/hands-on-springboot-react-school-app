b17070c435128b18d14d5a82ce2c6d1a6a27959c
rafaelturse <rafaelturse@hotmail.com>
2021-09-09 17:50:47 -0400

    helloReact

---
+++ simpleschool-app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import 'bootswatch/dist/flatly/bootstrap.css'
+
+class App extends React.Component {
+  render() {
+    return (
+        <buton type="button" className="btn btn-primary">
+          Primary
+        </buton>
+      )
+  }
+}
+
+export default App;
